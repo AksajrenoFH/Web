5d913255cf7437b5561d981b4b27e31e7c93f203
Aksajreno FH <146636998+AksajrenoFH@users.noreply.github.com>
2024-11-06 06:38:18 +0700

    Add files via upload

---
+++ !Script_some1.js
@@ -0,0 +1,13 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
+// function darkmode() {
+//     localStorage.setItem('dark-mode',this.checked);
+//     if(this.checked) {
+//         body.classList.add('dark-mode')
+//     }
+//     else {
+//         body.classList.remove('dark-mode')
+//     }
+// }
